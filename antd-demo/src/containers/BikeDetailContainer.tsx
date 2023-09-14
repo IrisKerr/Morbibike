9c4c9cb6ee4273d65f9f8bad1ab8b9309116d24f
Samuel HASSID <samuel@supplement-bacon.com>
2023-09-14 12:26:51 +0200

    Adapt to new reducer

--- antd-demo/src/containers/BikeDetailContainer.tsx
+++ antd-demo/src/containers/BikeDetailContainer.tsx
@@ -1,39 +1,25 @@
-import React from 'react'
 import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { RootState } from '../store/store'
-
-// import { initialBikes } from "../data/initialData"
-import { Velo } from '../models/types'
-import { Typography } from 'antd'
+import { Space, Typography } from 'antd'
+import { selectBikeById } from '../store/reducers/bikeSlice'
+import { useAppSelector } from '../store/hooks'
 import { Bike } from '../components/bike/Bike'
-const { Title } = Typography
+const { Title, Text } = Typography
 
-const BikeDetailContainer: React.FC = () => {
-  const bikes = useSelector((state: RootState) => state.bikes)
-  console.log('détail du vélo du store', bikes)
-  // Utilisez useParams() pour extraire l'ID du vélo à partir de l'URL
-  const { id } = useParams<{ id?: string }>()
-
-  if (typeof id === 'undefined') {
-    return <div>Identifiant de vélo non spécifié</div>
-  }
-
-  const bikeId = parseInt(id, 10)
-
-  const selectedBike: Velo | undefined = bikes.find(
-    (bike) => bike.id === bikeId
-  )
-
-  if (!selectedBike) {
-    return <div>Vélo non trouvé</div>
-  }
-
+const BikeDetailContainer = () => {
+  const id = useParams<{ id?: string }>()
+  const selectedBike = useAppSelector(selectBikeById(Number(id))) //On met le find dans le reducer en creant un custom selector pour reuse le code plus facilement
+  // Ne pas utiliser parseInt mais plutot un cast dans le type souhiaté ex : Number("1") = 1 et String(1) = "1"
   return (
-    <div>
-      <Title level={3}>Détails du vélo</Title>
-      <Bike.DetailsCard bike={selectedBike} />
-    </div>
+    // on utilise un ternaire qui test si selected bike existe (https://www.pierre-giraud.com/javascript-apprendre-coder-cours/operateur-ternaire/)
+    selectedBike ? ( //Condition en ternaire true
+      <Space direction="vertical">
+        <Title level={3}>Détails du vélo</Title>
+        <Bike.DetailsCard bike={selectedBike} />
+      </Space>
+    ) : (
+      //Condition en ternaire false
+      <Text>Vélo ou id non spécifié</Text> // possiblement utiliser le composant ant design 'result' en type error
+    )
   )
 }
 
