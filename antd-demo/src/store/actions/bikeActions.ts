37875cb927cb410d9388fbf4c9a81c0433c25993
IrisKerr <iris.kerr@hotmail.com>
2023-09-14 10:36:54 +0200

    deleteBike editBike reducer/action

--- antd-demo/src/store/actions/bikeActions.ts
+++ antd-demo/src/store/actions/bikeActions.ts
@@ -1,12 +1,27 @@
 // bikeActions.ts
 import { Dispatch } from 'redux'
-import { addBike } from '../reducers/bikeSlice'
+import { addBike, editBike, deleteBike } from '../reducers/bikeSlice'
 import { initialBikes } from '../../data/initialData' // Importez les données initiales
 
+// Action pour ajouter un vélo
 export const loadInitialData = () => {
   return async (dispatch: Dispatch) => {
     initialBikes.forEach((bike) => {
       dispatch(addBike(bike))
     })
+  }
+}
+
+// Action pour éditer un vélo
+export const editBikeAction = (id: number, updatedBike: Velo) => {
+  return (dispatch: Dispatch) => {
+    dispatch(editBike({ id, updatedBike }))
+  }
+}
+
+// Action pour supprimer un vélo
+export const deleteBikeAction = (id: number) => {
+  return (dispatch: Dispatch) => {
+    dispatch(deleteBike(id))
   }
 }
--- antd-demo/src/store/reducers/bikeSlice.ts
+++ antd-demo/src/store/reducers/bikeSlice.ts
@@ -1,19 +1,33 @@
 // bikeSlice.ts
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { Velo } from '../../models/types' // Assurez-vous d'importer les types nécessaires
+import { Velo } from '../../models/types' // Import du type vélo
 import { initialBikes } from '../../data/initialData'
 
 const bikeSlice = createSlice({
   name: 'bikes',
-  initialState: initialBikes as Velo[], // Utilisez initialBikes comme état initial
+  initialState: initialBikes as Velo[], // initialBikes = état initial
   reducers: {
     addBike: (state, action: PayloadAction<Velo>) => {
       state.push(action.payload) // Ajoute un nouveau vélo à l'état
     },
-    // Définissez d'autres actions de gestion des vélos si nécessaire
+    editBike: (
+      state,
+      action: PayloadAction<{ id: number; updatedBike: Velo }>
+    ) => {
+      const { id, updatedBike } = action.payload
+      const bikeIndex = state.findIndex((bike) => bike.id === id)
+      if (bikeIndex !== -1) {
+        state[bikeIndex] = updatedBike
+      }
+    },
+    deleteBike: (state, action: PayloadAction<number>) => {
+      const bikeIdToDelete = action.payload
+      const newState = state.filter((bike) => bike.id !== bikeIdToDelete)
+      return newState
+    },
   },
 })
 
-export const { addBike } = bikeSlice.actions // Exposez les actions pour être utilisées ailleurs
+export const { addBike, editBike, deleteBike } = bikeSlice.actions // Exposez les actions pour être utilisées ailleurs
 
 export default bikeSlice.reducer // Exportez le réducteur
