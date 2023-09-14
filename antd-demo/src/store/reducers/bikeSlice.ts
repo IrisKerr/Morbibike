8881c5cee734c538f864928a7b9ff5a912021a8f
Samuel HASSID <samuel@supplement-bacon.com>
2023-09-14 12:23:34 +0200

    Refactoring bike reducer

--- antd-demo/src/store/reducers/bikeSlice.ts
+++ antd-demo/src/store/reducers/bikeSlice.ts
@@ -1,18 +1,37 @@
-// bikeSlice.ts
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { Velo } from '../../models/types' // Assurez-vous d'importer les types nécessaires
+import { createSelector, createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { Velo } from '../../models/types'
 import { initialBikes } from '../../data/initialData'
+import { RootState } from '../store'
+
+interface initialStateType {
+  bikes: Velo[]
+}
+const initialState: initialStateType = {
+  bikes: initialBikes,
+}
 
 const bikeSlice = createSlice({
   name: 'bikes',
-  initialState: initialBikes as Velo[], // Utilisez initialBikes comme état initial
+  initialState: initialState, // Utilisez initialBikes comme état initial
   reducers: {
     addBike: (state, action: PayloadAction<Velo>) => {
-      state.push(action.payload) // Ajoute un nouveau vélo à l'état
+      state.bikes = [...state.bikes, action.payload]
     },
-    // Définissez d'autres actions de gestion des vélos si nécessaire
   },
 })
+
+/**
+ * Selectors
+ */
+const selectRawItems: (state: RootState) => Velo[] = (state: RootState) =>
+  state[bikeSlice.name].bikes
+
+export const selectBikeById = (
+  id: number // Exportez le selecteur par id
+) =>
+  createSelector([selectRawItems], (items) =>
+    items.find((bike) => bike.id === id)
+  )
 
 export const { addBike } = bikeSlice.actions // Exposez les actions pour être utilisées ailleurs
 
